2e6eb7128053d2759221345ee23cb0247f287164
kiyoshisatoshy <kiyoshisatoshy@gmail.com>
2022-04-22 15:55:07 +0300

    Initial project and dependencies

---
+++ app/src/main/java/com/crownedjester/soft/waterdrinkremainder/WaterReminderApp.kt
@@ -0,0 +1,8 @@
+package com.crownedjester.soft.waterdrinkremainder
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class WaterReminderApp : Application() {
+}
